4b5a74d6b2f3b97e06620191e3085f34d877325d
TheSameCat <thesamecat@proton.me>
2023-04-06 23:39:43 -0500

    Move some things, remove redundant code, use library thats helpful.

--- NostrLib/RelayManager.cs
+++ NostrLib/RelayManager.cs
@@ -2,4 +2,57 @@
 
 public class RelayManager
 {
+    private readonly Dictionary<string, NNostr.Client.NostrClient> _clients = new();
+
+    public bool AddRelay(string uri)
+    {
+        try
+        {
+            var relay = new NNostr.Client.NostrClient(new Uri(uri));
+            _clients.Add(uri, relay);
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.Diagnostics.Trace.TraceError("Could not add " + uri + " to the relay list.");
+            return false;
+        }
+    }
+
+    public bool ConnectAll()
+    {
+        var didOneFail = false;
+        foreach (var relay in _clients)
+        {
+            try
+            {
+                relay.Value.Connect();
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.TraceError("Failed to connect to " + relay.Value);
+                didOneFail = true;
+            }
+        }
+
+        return didOneFail;
+    }
+
+    public bool Connect(string uri)
+    {
+        var didFail = false;
+
+        try
+        {
+            _clients[uri].Connect();
+            
+        }
+        catch (Exception e)
+        {
+            System.Diagnostics.Trace.TraceError("Failed to connect to " + uri);
+            didFail = true;
+        }
+
+        return didFail;
+    }
 }
--- NostrLib/SqliteContext.cs
+++ NostrLib/SqliteContext.cs
@@ -5,7 +5,7 @@ namespace NostrLib;
 
 public class MyDbContext : DbContext
 {
-    public DbSet<Event> Events { get; set; }
+    public DbSet<NNostr.Client.NostrEvent> Events { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
