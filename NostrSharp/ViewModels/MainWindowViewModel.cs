5b0a57174e19a8018cdb63e397a07eedaba811cd
TheSameCat <thesamecat@proton.me>
2023-04-06 20:34:22 -0500

    Initial commit

---
+++ NostrSharp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace NostrSharp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ NostrSharp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace NostrSharp.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
