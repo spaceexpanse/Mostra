7612f6333189d8310ec29f42093b071b869412a4
TheSameCat <thesamecat@proton.me>
2023-04-17 13:49:12 -0500

    Add dummy tester for simple testing

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using NostrLib;
+
+RelayConnection relayConnection = new RelayConnection("wss://ws.postman-echo.com/raw");
+
+await relayConnection.ConnectAsync();
+Console.Write("Enter a message: ");
+var message = Console.ReadLine();
+await relayConnection.SendMessageAsync(!string.IsNullOrEmpty(message) ? message : "Hello World!");
+var response = await relayConnection.ReceiveMessageAsync();
+Console.WriteLine(response);
